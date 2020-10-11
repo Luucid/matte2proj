34f2c96e2bc4151934be01718c0f9dece77c0ef9
lucid <54083739+Luucid@users.noreply.github.com>
2020-10-12 01:00:43 +0200

    added prod, add, sub and working on determ

--- lucidMatrixMain.py
+++ lucidMatrixMain.py
@@ -16,20 +16,21 @@ class MatrixCalcs():
         #preps#
         self.prod = 0
         self.row = 0
+        self.det = 0
         
         
         
      
-    def setMats(self, a, b):
+    def __setMats(self, a, b = [0,0]):
         self.mat1 = a
         self.mat2 = b
-        self.setShape()
-        
-        if(self.m1 != self.m2):                                               
-            print("wrong matrix dimentions.")
-            return 0
-
-    def setShape(self):
+        self.__setShape()
+     
+    def __genMat(self, d): 
+        return np.array([[0]*d[1]]*d[0]) #return NxM array containing zeros.
+    
+    
+    def __setShape(self):
         
         a = np.shape(self.mat1)
         b = np.shape(self.mat2)
@@ -47,10 +48,10 @@ class MatrixCalcs():
         self.row = np.array([0]*self.m2) 
               
     
-    def getProdShape(self):
-        a = np.shape(self.prod)
+    def __getShape(self, m):
+        a = np.shape(m)
         if(len(a) <= 1):
-            a = np.shape([self.prod])
+            a = np.shape([m])
         return a
   
     
@@ -64,14 +65,43 @@ class MatrixCalcs():
             self.row[i] = self.mat2[i][c]
         return self.row
     
-    def __returnMatrix(self):
-        if (self.getProdShape()[0] == 1):
-            return self.prod[0]  
-        return self.prod
+    def __returnMatrix(self, m):
+        if (self.__getShape(m)[0] == 1):
+            return m[0]  
+        return m
+    
+    def __errorCheck(self, mode):
+        if(mode == "mult"):
+            if(self.m1 != self.m2):                                               
+                print("ERROR: Wrong matrix dimentions.")
+                return 1
+            
+        if(mode == "add"):
+            if(self.__getShape(self.mat1) != self.__getShape(self.mat2)):                                               
+                print("ERROR: Both matrixes must have the same dimentions.")
+                return 1
+            
+        if(mode == "sub"):
+            if(self.__getShape(self.mat1) != self.__getShape(self.mat2)):                                               
+                print("ERROR: Both matrixes must have the same dimentions.")
+                return 1   
+         
+        if(mode == "det"):
+            s = self.__getShape(self.mat1)
+            if(s[0] != s[1]):                                               
+                print("ERROR: Not a square matrix.")
+                return 1
+            
+        return 0
+            
+    
+    
     
     def matMult(self, a, b):     
-        self.setMats(a, b)
-
+        self.__setMats(a, b)
+        if(self.__errorCheck("mult")): 
+            return 0
+   
         for i in range(self.n):
             for j in range(self.k):
                 if(self.n <= 1):
@@ -79,17 +109,119 @@ class MatrixCalcs():
                 else:
                     self.prod[i][j] = self.__rowXcol(self.mat1[i], self.__genRow(j))
 
-        return self.__returnMatrix()
+        return self.__returnMatrix(self.prod)
     
   
+    def matAdd(self, a, b):
+        self.__setMats(a, b)
+        if(self.__errorCheck("add")):
+            return 0
+    
+        matSum = self.__genMat(self.__getShape(self.mat1))
+        
+        for i in range(self.n):
+            for j in range(self.m1):
+                if(self.n <= 1):
+                    matSum[i][j] = (self.mat1[j] + self.mat2[j])
+                else:
+                    matSum[i][j] = (self.mat1[i][j] + self.mat2[i][j])
+                
+        return self.__returnMatrix(matSum)
+    
+    
+    
+    def matSub(self, a, b):
+        self.__setMats(a, b)
+        if(self.__errorCheck("sub")): 
+            return 0
+        
+        matSum = self.__genMat(self.__getShape(self.mat1))
+        
+        for i in range(self.n):
+            for j in range(self.m1):
+                if(self.n <= 1):
+                    matSum[i][j] = (self.mat1[j] - self.mat2[j] )
+                else:
+                    matSum[i][j] = (self.mat1[i][j] - self.mat2[i][j])
+                
+        return self.__returnMatrix(matSum)
+        
+        
+  
+            
+     
+        
+    def __det3x(self, m):
+        a = (m[0][0]) * ((m[1][1]*m[2][2]) - (m[1][2]*m[2][1]))
+        b = (m[0][1]) * ((m[1][0]*m[2][2]) - (m[1][2]*m[2][0]))
+        c = (m[0][2]) * ((m[1][0]*m[2][1]) - (m[1][1]*m[2][0])) 
+        return (a - b + c)
+        
+    
+    def __shrinkDet(self, m):
+        d = self.__getShape(m)  
+        
+                
+            
+        
+        
+        
+    
+    def __detRek(self, m):
+        if(self.__getShape(m)[0] > 3):
+            self.__shrinkDet(m)
+            
+        
+      
        
         
-    
-    
-    def matPrint(self):   
-        print(self.mat1, "*", self.mat2)
-        print("(%dx%d)*(%dx%d)" % (self.n, self.m1, self.m2, self.k))
+    def matDet(self, a):
+        self.__setMats(a)
+        if(self.__errorCheck("det")): 
+            return 0
         
+        self.__detRek(self.mat1)
+        
+        # self.__det3x(self.mat1)
+        
+        
+        
+        
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    
+        
+    
 
 
 
@@ -97,7 +229,8 @@ class MatrixCalcs():
 
 
 
-
+    
+    
 
 
 
--- matte2.py
+++ matte2.py
@@ -2,30 +2,47 @@
 import lucidMatrixMain as lm
 
 # Ta imot to matriser og gi produktet av dem som output. CHECK
-# Ta imot to matriser og gi summen av dem.
+# Ta imot to matriser og gi summen av dem. CHECK
 # Ta imot to matriser og finne differansen mellom dem
+
 # Finne determinanten til en matrise
 # Finne den redusert trappeform til en matrise (Gauss-Jordan eliminasjon)
 
 
 
-    
-   
-        
-
-        
-
-
-
-
-
-m1 = [1,2]
-m2 = [[1,2,3],[4,5,6]]    
 
 mx = lm.MatrixCalcs()
 
-prod = mx.matMult(m1, m2)
-print(prod)
+m0 = [[1,2,3,4],[5,6,7, 8],[9, 10, 11, 12], [13,14,15,16]]
+m1 = [[1,2],[3,4]]   
+m2 = [[5,6],[7,8]]    
+m3 = [1,2]
+m4 = [3,4]  
+
+ma = [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
+mb = [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
+
+
+
+
+
+
+
+
+
+
+
+
+prod = mx.matMult(m2, m1)
+add = mx.matAdd(ma, mb)
+sub = mx.matSub(m3, m4)
+det = mx.matDet(m0)
+
+# print(prod)
+# print("\n")
+# print(add)
+# print("\n")
+# print(sub)
 
 
     
