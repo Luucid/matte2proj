58a1e42b22b0e510b3631b3c5c83ce935c692f48
lucid <54083739+Luucid@users.noreply.github.com>
2020-10-15 09:48:33 +0200

    ..

--- matte2.py
+++ matte2.py
@@ -4,20 +4,15 @@ import lucidMatrixMain as lm
 # Ta imot to matriser og gi summen av dem. CHECK
 # Ta imot to matriser og finne differansen mellom dem CHECK
 # Finne determinanten til en matrise CHECK
+# Finne den redusert trappeform til en matrise (Gauss-Jordan eliminasjon) CHECK
 
-# Finne den redusert trappeform til en matrise (Gauss-Jordan eliminasjon)
-
-
-
-
-mx = lm.MatrixCalcs()
 
 m0 = [[-2,-1,2,1],
       [5,5,-5,5],
       [6,8,2,4], 
       [3,6,9,12]]
 
-test = [[-1,2,3],
+test = [[2,4,6],
         [4,5,6],
         [7,8,9]]
 
@@ -26,24 +21,42 @@ m2 = [[5,6],[7,8]]
 m3 = [1,2]
 m4 = [3,4]  
 
-ms = [[1, 1],
-      [2, 3]]
+ms = [[4, 4],
+      [2, 2]]
 
 ma = [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
 mb = [[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]]
 
 
-prod = mx.matMult(m2, m1)
-add = mx.matAdd(ma, mb)
-sub = mx.matSub(m3, m4)
-det = mx.matDet(m0)
 
-print(det)
-# print(prod)
-# print("\n")
-# print(add)
-# print("\n")
-# print(sub)
+mA = lm.MatrixCalcs(test, "3x3 matrix")
+mB = lm.MatrixCalcs(test, "3x3 matrix")
+print(mA)
+print(mB)
+
+product = mA*mB
+print(product)
+
+
+
+add = mA+mB
+print(add)
+
+sub = mA-mB
+print(sub)
+
+
+
+print(mA)
+print(mB)
+
+gaus = mA.gausJordan()
+print(gaus)
+
+
+print(mA)
+print(mB)
+
 
 
     
